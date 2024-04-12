fd0c48a923c4174464115b4f7e6c20b5b4d62014
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-04-12 16:56:37 +0530

    added user router in app file

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const dbConnection = require('./database/dbConnection.js');
+const {errorMiddleware} = require('./middlewares/error.js');
+const userRouter = require('./Routes/userRouter.js');
 
 dotenv.config({path:'./config/config.env'});
 
@@ -18,5 +21,11 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+app.use("/api/v1",userRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
 
 module.exports=app;
