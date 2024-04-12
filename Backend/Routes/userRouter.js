2f2fe7697ece8c7b36e1a0e1bc676e04dc547f2c
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-04-12 18:23:45 +0530

    added logout functionality

--- Backend/Routes/userRouter.js
+++ Backend/Routes/userRouter.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const {register,login} = require('../controllers/userController.js');
+const {register,login,logout} = require('../controllers/userController.js');
+const {isAuthenticated} =require('../middlewares/auth.js')
+
 
 const router=express.Router();
 
+
 router.post('/register',register);
 router.post('/login',login);
+router.get('/logout',isAuthenticated,logout);
 
 
 module.exports=router;
