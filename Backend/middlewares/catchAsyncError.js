e701affae681e25fd8a738c59ffdbd791f72213b
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-04-12 12:55:02 +0530

    catchAsyncfuntion created

---
+++ Backend/middlewares/catchAsyncError.js
@@ -0,0 +1,9 @@
+const catchAysncError=(func)=>{
+	return (req,res,next)=>{
+		Promise.resolve(func(req,res,next)).catch((err)=>{
+			console.log(err);
+			next(err)});
+	}
+}
+
+module.exports=catchAysncError;
