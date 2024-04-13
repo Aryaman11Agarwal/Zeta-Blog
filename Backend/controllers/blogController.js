1708a97097cdf7a8a7ef32f250a307776c0519a6
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-04-13 14:37:28 +0530

    working on post blog function

---
+++ Backend/controllers/blogController.js
@@ -0,0 +1,62 @@
+const catchAsyncError = require("../middlewares/catchAsyncError");
+const { ErrorHandler } = require("../middlewares/error");
+const blogModel = require("../models/blogModel");
+const cloudinary = require("cloudinary");
+
+const blogPost = catchAsyncError(async(req, res, next) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return next(new ErrorHandler("Blog main image is mandatory", 400));
+  }
+  const { mainImage, paraOneImage, paraTwoImage, paraThreeImage } = req.files;
+  if (!mainImage) {
+    return next(new ErrorHandler("Blog main image is mandatory", 400));
+  }
+  const allowedFormats = ["image/png", "image/jpeg", "image/webp"];
+  if (
+    (!allowedFormats.includes(mainImage.mimetype))||
+    (paraOneImage && !allowedFormats.includes(paraOneImage.mimetype)) ||
+    (paraTwoImage && !allowedFormats.includes(paraTwoImage.mimetype)) ||
+    (paraThreeImage && !allowedFormats.includes(paraThreeImage.mimetype))
+  ) {
+    return next(
+      new ErrorHandler("Upload main image in webp,jpeg or png format only", 400)
+    );
+  }
+
+  const {
+    title,
+    intro,
+    paraOneDescription,
+    paraOneTitle,
+    paraTwoDescription,
+    paraTwoTitle,
+    paraThreeDescription,
+    paraThreeTitle,
+    category,
+    published,
+  } = req.body;
+
+  const createdBy = req.user._id;
+  const authorName = req.user.name;
+  const authorAvatar = req.user.avatar.url;
+
+  if(!title||!intro||!category){
+    return next("Title,category and intro are required fields",400);
+  }
+
+  const uploadPromises = [
+    cloudinary.uploader.upload(mainImage.tempFilePath),
+    paraOneImage
+      ? cloudinary.uploader.upload(paraOneImage.tempFilePath)
+      : Promise.resolve(null),
+    paraTwoImage
+      ? cloudinary.uploader.upload(paraTwoImage.tempFilePath)
+      : Promise.resolve(null),
+    paraThreeImage
+      ? cloudinary.uploader.upload(paraThreeImage.tempFilePath)
+      : Promise.resolve(null),
+  ];
+
+  const [mainImageRes, paraOneImageRes, paraTwoImageRes, paraThreeImageRes] =
+    await Promise.all(uploadPromises);
+});
