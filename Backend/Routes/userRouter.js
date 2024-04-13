a4d9d259347208da18d3dc92c01cd0363ac605a1
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-04-13 11:28:51 +0530

    added myprofile and author routes

--- Backend/Routes/userRouter.js
+++ Backend/Routes/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {register,login,logout} = require('../controllers/userController.js');
+const {register,login,logout,getMyProfile,getAllAuthors} = require('../controllers/userController.js');
 const {isAuthenticated} =require('../middlewares/auth.js')
 
 
@@ -9,6 +9,8 @@ const router=express.Router();
 router.post('/register',register);
 router.post('/login',login);
 router.get('/logout',isAuthenticated,logout);
+router.get('/myProfile',isAuthenticated,getMyProfile);
+router.get('/authors',getAllAuthors);
 
 
 module.exports=router;
