9057f2bea1786ab26470dc2d1875bf941671982a
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-04-13 15:52:26 +0530

    Get all blogs functionality added

--- Backend/Routes/blogRouter.js
+++ Backend/Routes/blogRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const {isAuthenticated,isAuthorized} = require('../middlewares/auth');
-const {blogPost} = require('../controllers/blogController');
+const {blogPost,deletePost,getAllBlogs} = require('../controllers/blogController');
 
 const router=express.Router();
 
 router.post('/post',isAuthenticated,isAuthorized("Author"),blogPost);
+router.delete('/delete/:id',isAuthenticated,isAuthorized("Author"),deletePost)
+router.get('/all',getAllBlogs)
 
 module.exports=router
