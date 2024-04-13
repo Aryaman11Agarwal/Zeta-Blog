a693b64d03234f2244baf14aa03c34a17c55e6f6
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-04-13 17:21:52 +0530

    added blog update route

--- Backend/Routes/blogRouter.js
+++ Backend/Routes/blogRouter.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const {isAuthenticated,isAuthorized} = require('../middlewares/auth');
-const {blogPost,deletePost,getAllBlogs} = require('../controllers/blogController');
+const {blogPost,deletePost,getAllBlogs,getSingleBlog,getMyBlogs,updateBlog} = require('../controllers/blogController');
 
 const router=express.Router();
 
 router.post('/post',isAuthenticated,isAuthorized("Author"),blogPost);
 router.delete('/delete/:id',isAuthenticated,isAuthorized("Author"),deletePost)
 router.get('/all',getAllBlogs)
+router.get('/singleblog/:id',isAuthenticated,getSingleBlog)
+router.get('/myBlogs',isAuthenticated,isAuthorized("Author"),getMyBlogs)
+router.put('/update/:id',isAuthenticated,isAuthorized("Author"),updateBlog)
 
 module.exports=router
