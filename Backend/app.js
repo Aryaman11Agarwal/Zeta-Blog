34b92635d31b5a5c61761f76f478b34e7943fee1
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-04-20 09:06:13 +0530

    cors error fixed

--- Backend/app.js
+++ Backend/app.js
@@ -15,10 +15,10 @@ dotenv.config({path:'./config/config.env'});
 const app=express();
 
 app.use(cors({
-    origin:[process.env.FRONTEND_URL],
-    methods:['GET','POST','PUT','DELETE'],
-    credentials:true,
-}))
+    origin: [process.env.FRONTEND_URL],
+    methods:['GET','PUT','POST','DELETE'],
+    credentials:true
+    }));
 
 app.use(cookieParser());
 app.use(express.json());
